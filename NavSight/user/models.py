0756870c5bce4e2fbf7edb9dbe7f0ee58f48c19e
Armaan Jagirdar <armaanjagirdar05@gmail.com>
2024-07-05 03:01:44 +0530

    user models created

--- NavSight/user/models.py
+++ NavSight/user/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class user_details(models.Model):
+    name = models.CharField(max_length=100)
+    age = models.PositiveIntegerField()
+    phone = models.PositiveIntegerField()
+    email = models.CharField(max_length=50)
+
+
+    
